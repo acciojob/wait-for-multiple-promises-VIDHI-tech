61161c3e020f92ac5da19d6ebd47954219550c03
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-05-30 21:11:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,56 +1,61 @@
-// Select the table element
-const table = document.getElementById('myTable');
+let resolvedPromises = [];
+// {name: "Promise1", time_taken: 2s}
+let minTime, maxTime;
 
-// Add a row with the loading text
-const loadingRow = table.insertRow();
-const loadingCell = loadingRow.insertCell();
-loadingCell.colSpan = 2;
-loadingCell.textContent = 'Loading...';
-
-// Function to generate a random time between min and max (inclusive)
-function getRandomTime(min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min) * 1000; // Convert to milliseconds
+function addPromiseStatus(name, start, end) {
+	if (!minTime) minTime = start;
+	maxTime = end;
+	resolvedPromises.push({
+		name,
+		time: (end - start) / 1000
+	})
 }
 
-// Create an array of Promises
-const promises = Array.from({ length: 3 }, (_, index) =>
-  new Promise(resolve => {
-    const time = getRandomTime(1, 3);
-    setTimeout(() => {
-      resolve(time);
-    }, time);
-  })
-);
+let start1 = new Date().getTime(); // milliseconds from 1970 Jan 1
+let promise1 = new Promise((resolve) => {
+	setTimeout(() => {
+		resolve();
+		let end1 = new Date().getTime(); // milliseconds from 1970 Jan 1 
+		addPromiseStatus("Promise1", start1, end1)
+	}, 2000)
+})
 
-// Wait for all Promises to resolve
-Promise.all(promises)
-  .then(times => {
-    // Remove the loading row
-    table.deleteRow(loadingRow.rowIndex);
+let start2 = new Date().getTime();
+let promise2 = new Promise((resolve) => {
+	setTimeout(() => {
+		resolve();
+		let end2 = new Date().getTime();
+		addPromiseStatus("Promise2", start2, end2);
+	}, 1000)
+})
 
-    // Iterate over the resolved times and populate the table
-    times.forEach((time, index) => {
-      const row = table.insertRow();
-      const firstColumn = row.insertCell();
-      const secondColumn = row.insertCell();
+let start3 = new Date().getTime();
+let promise3 = new Promise((resolve) => {
+	setTimeout(() => {
+		resolve();
+		let end3 = new Date().getTime();
+		addPromiseStatus("Promise3", start3, end3)
+	}, 3000)
+})
 
-      // Populate the columns
-      firstColumn.textContent = `Promise ${index + 1}`;
-      secondColumn.textContent = (time / 1000).toFixed(3);
-    });
+let finalPromise = Promise.all([promise1, promise2, promise3]);
 
-    // Calculate the total time
-    const totalTime = times.reduce((acc, time) => acc + time, 0);
+finalPromise.then(() => {
+	// console.log(resolvedPromises);
+	addtoUI(resolvedPromises);
+})
 
-    // Add the total row
-    const totalRow = table.insertRow();
-    const totalFirstColumn = totalRow.insertCell();
-    const totalSecondColumn = totalRow.insertCell();
+const table = document.getElementsByTagName("table")[0];
+const loadingRow = document.getElementById("loading-row");
 
-    // Populate the total row
-    totalFirstColumn.textContent = 'Total';
-    totalSecondColumn.textContent = (totalTime / 1000).toFixed(3);
-  })
-  .catch(error => {
-    console.error('An error occurred:', error);
-  });
+function addtoUI(list) {
+	loadingRow.remove();
+	for (let i = 0; i < list.length; i++) {
+		let tr = document.createElement("tr");
+		tr.innerHTML = `<td>${list[i].name}</td><td>${list[i].time}</td>`;
+		table.append(tr);
+	}
+	let tr = document.createElement("tr");
+	tr.innerHTML = `<td>Total</td><td>${(maxTime - minTime)/1000}</td>`
+	table.append(tr);
+}
