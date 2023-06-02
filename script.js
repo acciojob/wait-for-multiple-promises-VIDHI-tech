0840f135310e9725ec705388019f29c69980af8f
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-02 09:12:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,47 @@
+// function getRandomDelay() {
+// 	return Math.floor(Math.random() * 3000) + 1000; // Random time between 1 and 3 seconds in milliseconds
+// }
+
+// function createPromise(delay) {
+//   return new Promise(resolve => {
+//     setTimeout(() => {
+//       resolve(delay / 1000); // Resolving with time taken in seconds
+//     }, delay);
+//   });
+// }
+
+// async function populateTable() {
+//   const promises = [
+//     createPromise(getRandomDelay()),
+//     createPromise(getRandomDelay()),
+//     createPromise(getRandomDelay())
+//   ];
+
+// const results = await Promise.all(promises);
+// const loadingRow = document.getElementById('loading');
+// const table = loadingRow.parentNode;
+
+// loadingRow.remove();
+
+// results.forEach((time, index) => {
+//   const row = table.insertRow();
+//   const promiseCell = row.insertCell();
+//   const timeCell = row.insertCell();
+// 	promiseCell.textContent = `Promise ${index + 1}`;
+//     timeCell.textContent = time.toFixed(3);
+// });
+
+// const totalRow = table.insertRow();
+// const totalPromiseCell = totalRow.insertCell();
+// const totalTimeCell = totalRow.insertCell();
+
+//   totalPromiseCell.textContent = 'Total';
+//   totalTimeCell.textContent = results.reduce((sum, time) => sum + time, 0).toFixed(3);
+// }
+
+// populateTable();
 function getRandomDelay() {
-	return Math.floor(Math.random() * 3000) + 1000; // Random time between 1 and 3 seconds in milliseconds
+  return Math.floor(Math.random() * 3000) + 1000; // Random time between 1 and 3 seconds in milliseconds
 }
 
 function createPromise(delay) {
@@ -17,23 +59,24 @@ async function populateTable() {
     createPromise(getRandomDelay())
   ];
 
-const results = await Promise.all(promises);
-const loadingRow = document.getElementById('loading');
-const table = loadingRow.parentNode;
+  const results = await Promise.all(promises);
+  const loadingRow = document.getElementById('loading');
+  const table = loadingRow.parentNode;
 
-loadingRow.remove();
+  loadingRow.remove();
 
-results.forEach((time, index) => {
-  const row = table.insertRow();
-  const promiseCell = row.insertCell();
-  const timeCell = row.insertCell();
-	promiseCell.textContent = `Promise ${index + 1}`;
+  results.forEach((time, index) => {
+    const row = table.insertRow();
+    const promiseCell = row.insertCell();
+    const timeCell = row.insertCell();
+
+    promiseCell.textContent = `Promise ${index + 1}`;
     timeCell.textContent = time.toFixed(3);
-});
+  });
 
-const totalRow = table.insertRow();
-const totalPromiseCell = totalRow.insertCell();
-const totalTimeCell = totalRow.insertCell();
+  const totalRow = table.insertRow();
+  const totalPromiseCell = totalRow.insertCell();
+  const totalTimeCell = totalRow.insertCell();
 
   totalPromiseCell.textContent = 'Total';
   totalTimeCell.textContent = results.reduce((sum, time) => sum + time, 0).toFixed(3);
